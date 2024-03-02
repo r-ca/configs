5559fe5774faa1c9d4d1974df3d6785fcb78d10f
r-ca <nem@nem.x0.to>
2024-03-02 17:33:35 +0900

    デフォルトのマップを追加

--- common/nvim/lua/plugins/telescope.lua
+++ common/nvim/lua/plugins/telescope.lua
@@ -5,5 +5,12 @@ return {
         dependencies = {
             'nvim-lua/plenary.nvim',
         },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<Leader>ff', builtin.find_files)
+            vim.keymap.set('n', '<Leader>fg', builtin.live_grep)
+            vim.keymap.set('n', '<Leader>fb', builtin.buffers)
+            vim.keymap.set('n', '<Leader>fh', builtin.help_tags)
+        end
     }
 }
