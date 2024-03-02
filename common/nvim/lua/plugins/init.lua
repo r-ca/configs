fac075f801b63f1e3b26e32944ac31db1db5eadd
r-ca <nem@nem.x0.to>
2024-03-02 14:50:54 +0900

    WIP

--- common/nvim/lua/plugins/init.lua
+++ common/nvim/lua/plugins/init.lua
@@ -1,12 +1,13 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-          "MunifTanjim/nui.nvim",
-          -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    }
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup({
+        vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true }),
+      })
+    end,
+  }
 }
