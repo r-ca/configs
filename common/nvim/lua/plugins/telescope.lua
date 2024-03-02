31e08ad9227b3f84fe1ea155282388908ae3d9fd
r-ca <nem@nem.x0.to>
2024-03-02 17:35:27 +0900

    キーマップ変更

--- common/nvim/lua/plugins/telescope.lua
+++ common/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,8 @@ return {
         },
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<Leader>ff', builtin.find_files)
-            vim.keymap.set('n', '<Leader>fg', builtin.live_grep)
-            vim.keymap.set('n', '<Leader>fb', builtin.buffers)
-            vim.keymap.set('n', '<Leader>fh', builtin.help_tags)
+            vim.keymap.set('n', '<C-f>', builtin.find_files)
+            vim.keymap.set('n', '<C-g>', builtin.live_grep)
         end
     }
 }
