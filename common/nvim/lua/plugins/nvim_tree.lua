75bc839994224c4a10b93b6f40853a046db2b912
r-ca <nem@nem.x0.to>
2024-03-02 16:54:07 +0900

    WIP

--- common/nvim/lua/plugins/nvim_tree.lua
+++ common/nvim/lua/plugins/nvim_tree.lua
@@ -12,6 +12,11 @@ return {
 				view = {
 					width = 30,
 				},
+				on_attach = function(bufnr)
+					local api = require 'nvim-tree.api'
+					-- api.config.mappings.default_on_attach(bufnr)
+					vim.keymap.set('n', 't', api.tree.toggle_help, { noremap = true, silent = true, buffer = bufnr, nowait = true})
+				end
 			})
 			vim.api.nvim_set_keymap('n', 'f', ':NvimTreeToggle<CR>', { noremap = false, silent = true })
 		end
