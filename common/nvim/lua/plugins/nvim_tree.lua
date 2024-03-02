3e09ced3044c8200e9966a8e21ef32bf9c6af0ba
r-ca <nem@nem.x0.to>
2024-03-02 16:56:42 +0900

    fの単押しで切り替えれるように

--- common/nvim/lua/plugins/nvim_tree.lua
+++ common/nvim/lua/plugins/nvim_tree.lua
@@ -14,8 +14,8 @@ return {
 				},
 				on_attach = function(bufnr)
 					local api = require 'nvim-tree.api'
-					-- api.config.mappings.default_on_attach(bufnr)
-					vim.keymap.set('n', 't', api.tree.toggle_help, { noremap = true, silent = true, buffer = bufnr, nowait = true})
+					api.config.mappings.default_on_attach(bufnr)
+					vim.keymap.del('n', 'f', { buffer = bufnr })
 				end
 			})
 			vim.api.nvim_set_keymap('n', 'f', ':NvimTreeToggle<CR>', { noremap = false, silent = true })
