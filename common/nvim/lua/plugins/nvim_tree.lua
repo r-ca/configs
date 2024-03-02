b738259bbbb6143b12d0926035d69a027251e2ec
r-ca <nem@nem.x0.to>
2024-03-02 15:54:42 +0900

    WIP

--- common/nvim/lua/plugins/nvim_tree.lua
+++ common/nvim/lua/plugins/nvim_tree.lua
@@ -1,13 +1,20 @@
-require("nvim-tree").setup({
-    view = {
-        adaptive_size = true,
-        mappings = {
-            list = {
-                { key = "u", action = 'dir_up' },
-            },
-        },
-    },
-    filters = {
-        dotfiles = true,
-    },
-})
+return {
+	{
+		'nvim-tree/nvim-tree.lua',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons',
+		},
+		keys = {
+			{ "<leader>f", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file tree" },
+			{ "F", "<cmd>NvimTreeFindFile<CR>", desc = "Find file in tree" },
+		},
+		opts = {
+			view = {
+				adaptive_size = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		},
+	}
+}
