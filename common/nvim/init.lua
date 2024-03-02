570fe4f85b3194532c77ab50bc683a517bd58f2c
r-ca <nem@nem.x0.to>
2024-03-02 13:29:02 +0900

    WIP

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -11,3 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+        { import = "plugins.java" }
+    }
+})
