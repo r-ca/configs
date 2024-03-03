1ac12886c144ffef0ea89ec2b337f2cc801a85ad
r-ca <nem@nem.x0.to>
2024-03-03 09:55:35 +0900

    Fix

--- common/nvim/lua/plugins/coc.lua
+++ common/nvim/lua/plugins/coc.lua
@@ -9,8 +9,7 @@ return {
 		'neoclide/coc.nvim',
 		branch = 'release',
 		config = function()
-			vim.keymap.set('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "Space" : coc#refresh()', { silent = true, expr = true }) -- Use <C-Space> to trigger completion.
-			-- +Shift = back
+			vim.keymap.set('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', { silent = true, expr = true }) -- Use <C-Space> to trigger completion.
 			vim.keymap.set('i', '<S-TAB>', [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], { silent = true, expr = true }) -- Use <C-Space> to trigger completion.	
 			vim.keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], { silent = true, expr = true })
 		end
