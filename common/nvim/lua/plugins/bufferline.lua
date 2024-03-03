7afe3212108862cea50d8b4134a91d54cd0ea26d
r-ca <nem@nem.x0.to>
2024-03-03 10:28:28 +0900

    インデント訂正

--- common/nvim/lua/plugins/bufferline.lua
+++ common/nvim/lua/plugins/bufferline.lua
@@ -1,23 +1,23 @@
 return {
-    {
-        'akinsho/nvim-bufferline.lua',
-        config = function()
-            require('bufferline').setup {
-                options = {
-                    numbers = 'ordinal',
-                    close_command = 'bdelete! %d',
-                    right_mouse_command = 'bdelete! %d',
-                    middle_mouse_command = 'bdelete! %d',
-                    offsets = {
-                        {
-                            filetype = 'NvimTree',
-                            text = 'File Explorer',
-                            highlight = 'Directory',
-                            text_align = 'left'
-                        }
-                    }
-                }
-            }
-        end
-    }
+	{
+		'akinsho/nvim-bufferline.lua',
+		config = function()
+			require('bufferline').setup {
+				options = {
+					numbers = 'ordinal',
+					close_command = 'bdelete! %d',
+					right_mouse_command = 'bdelete! %d',
+					middle_mouse_command = 'bdelete! %d',
+					offsets = {
+						{
+							filetype = 'NvimTree',
+							text = 'File Explorer',
+							highlight = 'Directory',
+							text_align = 'left'
+						}
+					}
+				}
+			}
+		end
+	}
 }
