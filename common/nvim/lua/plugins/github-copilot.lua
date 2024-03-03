0ec7e3e49f9a0d9f52dc2b79268137e9fa44fd58
r-ca <nem@nem.x0.to>
2024-03-03 10:08:33 +0900

    Copilotのキーバインド変更 WIP

--- common/nvim/lua/plugins/github-copilot.lua
+++ common/nvim/lua/plugins/github-copilot.lua
@@ -2,5 +2,10 @@ return {
 	{
 		'github/copilot.vim',
 		lazy = false,
+		config = function()
+			require('github-copilot').setup({
+				vim.keymap.set('i', '<S-TAB>', 'copilot#Accept("<CR>")', { silent = true, expr = true }),
+			})
+		end
 	}
 }
