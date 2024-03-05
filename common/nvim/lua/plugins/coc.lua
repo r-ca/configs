0e846294a21822fc9c922fa7c3c640d037aa08af
r-ca <nem@nem.x0.to>
2024-03-05 21:21:29 +0900

    Fix indent

--- common/nvim/lua/plugins/coc.lua
+++ common/nvim/lua/plugins/coc.lua
@@ -11,9 +11,9 @@ return {
 		config = function()
 			vim.keymap.set('i', '<TAB>',
 				'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()',
-				{ noremap = true, silent = true, expr = true })                                                                                                           -- Use <C-Space> to trigger completion.
+				{ noremap = true, silent = true, expr = true }) -- Use <C-Space> to trigger completion.
 			vim.keymap.set('i', '<S-TAB>', 'coc#pum#visible() ? coc#pum#prev(1) : "<C-h>"',
-				{ silent = true, expr = true })                                                                                                                        -- Use <C-Space> to trigger completion.
+				{ silent = true, expr = true })     -- Use <C-Space> to trigger completion.
 			vim.keymap.set("i", "<cr>",
 				[[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
 				{ silent = true, expr = true })
