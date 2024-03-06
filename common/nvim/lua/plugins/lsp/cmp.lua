e4d82714e2068de85eef7b24833527c8497cc759
r-ca <nem@nem.x0.to>
2024-03-06 13:08:19 +0900

    Fix typo

--- common/nvim/lua/plugins/lsp/cmp.lua
+++ common/nvim/lua/plugins/lsp/cmp.lua
@@ -12,7 +12,7 @@ return {
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 		require('mason').setup()
-		require('mason-lsnconfig').setup()
+		require('mason-lspconfig').setup()
 		require('mason-lspconfig').setup_handlers {
 			function(server_name)
 				require('lspconfig')[server_name].setup {
