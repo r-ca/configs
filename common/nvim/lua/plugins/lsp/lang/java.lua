aba860108991f6ce6e32ffffb3a51a51868f4931
r-ca <nem@nem.x0.to>
2024-03-07 02:08:14 +0900

    WIP

--- common/nvim/lua/plugins/lsp/lang/java.lua
+++ common/nvim/lua/plugins/lsp/lang/java.lua
@@ -19,18 +19,23 @@ mason_registry = require("mason-registry")
 -- TODO: Fix this
 jdtls_path = '/home/rca/.local/share/nvim/mason/packages/jdtls'
 
+-- java_binary = java_bin()
+java_binary = '/usr/lib/jvm/java-21-openjdk/bin/java'
+
+capabilities = require('plugins.lsp.ext.handlers').capabilities
+
 local config = {
 	cmd = { 
-		java_bin(),		
+		java_binary,
 		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
 		'-Dosgi.bundles.defaultStartLevel=4',
 		'-Declipse.product=org.eclipse.jdt.ls.core.product',
-		'-Dlog.protocol=true',
-		'-Dlog.level=ALL',
+		--'-Dlog.protocol=true',
+		--'-Dlog.level=ALL',
 		'-Xmx1g',
 		'--add-modules=ALL-SYSTEM',
-		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
-		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    	'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+	    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
 
 		'-jar',
 		vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
@@ -39,7 +44,9 @@ local config = {
 		'-data',
 		workspace_dir,
 	},
-		
+
+	capabilities = capabilities,
+
 	root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 	settings = {
 		java = {
