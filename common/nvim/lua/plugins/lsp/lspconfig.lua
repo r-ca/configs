8bc03daa38758a2e8ff72a726f2647c148b05d1b
r-ca <nem@nem.x0.to>
2024-03-06 13:10:06 +0900

    Fix

--- common/nvim/lua/plugins/lsp/lspconfig.lua
+++ common/nvim/lua/plugins/lsp/lspconfig.lua
@@ -5,5 +5,16 @@ return {
 		{'neovim/nvim-lspconfig'},
 	},
 	config = function()
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+		require('mason').setup()
+		require('mason-lspconfig').setup()
+		require('mason-lspconfig').setup_handlers {
+			function(server_name)
+				require('lspconfig')[server_name].setup {
+					capabilities = capabilities,
+				}
+			end,
+		}
 	end
 }
