8281ab532c650bd2ea05addc39d563d3ef31bccc
r-ca <nem@nem.x0.to>
2024-03-06 15:21:44 +0900

    Fix

--- common/nvim/lua/plugins/lsp/cmp.lua
+++ common/nvim/lua/plugins/lsp/cmp.lua
@@ -10,12 +10,6 @@ return {
 	},
 	config = function()
 		require('cmp').setup({
-			snippet = {
-				expand = function(args)
-					require('luasnip').lsp_expand(args.body)
-				end,
-			},
-
 			sources = {
 				{ name = 'nvim_lsp' },
 				{ name = 'buffer' },
