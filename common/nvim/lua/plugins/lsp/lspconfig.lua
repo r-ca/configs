6c7d6c924bbd290a0c2c3ccf8c5e8c816fad7eed
r-ca <nem@nem.x0.to>
2024-03-06 12:55:57 +0900

    プラグイン追加

--- common/nvim/lua/plugins/lsp/lspconfig.lua
+++ common/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,5 +1,16 @@
 return {
-	'williamboman/mason.nvim',
 	'williamboman/mason-lspconfig.nvim',
-	'neovim/nvim-lspconfig',
+	dependencies = {
+		{'williamboman/mason.nvim'},
+		{'neovim/nvim-lspconfig'},
+		{'hrsh7th/nvim-cmp'},
+		{'hrsh7th/cmp-nvim-lsp'},
+		{'hrsh7th/cmp-buffer'},
+		{'hrsh7th/cmp-path'},
+		{'hrsh7th/vim-vsnip'},
+		{'hrsh7th/cmp-vsnip'},
+		{'onsails/lspkind-nvim'},
+	},
+	config = function()
+		
 }
