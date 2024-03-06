bd2d5b7f8dce82312525c9d8c45b1e5f623a5525
r-ca <nem@nem.x0.to>
2024-03-07 00:14:16 +0900

    WIP

--- common/nvim/lua/plugins/lsp/lang/java.lua
+++ common/nvim/lua/plugins/lsp/lang/java.lua
@@ -1,6 +1,12 @@
 local config = {
 	cmd = { '/home/rca/.local/share/nvim/mason/bin/jdtls' },
 	root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+	settings = {
+		java = {
+			signatureHelp = { enabled = true },
+
+		}
+	}
 }
 
 return {
