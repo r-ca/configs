052ba03ce57d199ad14b1ea7a192d32ebac9c3c3
r-ca <nem@nem.x0.to>
2024-03-08 05:44:43 +0900

    キーバインド変更

--- common/nvim/lua/plugins/lsp/ext/handlers.lua
+++ common/nvim/lua/plugins/lsp/ext/handlers.lua
@@ -46,7 +46,7 @@ M.on_attach = function(client, bufnr)
     set_hover_border(client)
 
 	-- Keybindings
-	vim.keymap.set('n', 'an', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = bufnr })
+	vim.keymap.set('n', '<Space>', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = false, buffer = bufnr })
 	vim.keymap.set('n', 'dj', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = bufnr })
 	vim.keymap.set('n', 'rf', '<cmd>lua vim.lsp.buf.formatting()<CR>', { buffer = bufnr })
 	vim.keymap.set('n', 'F', '<cmd>lua vim.lsp.buf.references()<CR>', { buffer = bufnr })
