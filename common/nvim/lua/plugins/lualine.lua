3ca7a227dda3fbe07bc8f50ea10d003cad2457d5
r-ca <nem@nem.x0.to>
2024-03-07 16:32:31 +0900

    接続されてるLSをLualineに表示するように

--- common/nvim/lua/plugins/lualine.lua
+++ common/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,20 @@
+local lsp_names = function()
+	local clients = {}
+	for _, client in ipairs(vim.lsp.get_active_clients { bufnr = 0 }) do
+		if client.name == 'null-ls' then
+			local sources = {}
+			for _, source in ipairs(require('null-ls.sources').get_available(vim.bo.filetype)) do
+				table.insert(sources, source.name)
+			end
+			table.insert(clients, 'null-ls(' .. table.concat(sources, ', ') .. ')')
+		else
+			table.insert(clients, client.name)
+		end
+	end
+	return ' ' .. table.concat(clients, ', ')
+end
+
+
 return {
 	{
 	'nvim-lualine/lualine.nvim',
@@ -16,7 +33,7 @@ return {
 				},
     				lualine_b = { 'filename', 'branch' },
     				lualine_c = { 'fileformat' },
-    				lualine_x = {},
+    				lualine_x = { lsp_names },
     				lualine_y = { 'filetype', 'progress' },
     				lualine_z = {
      					{ 'location', separator = { right = '' }, left_padding = 2 },
