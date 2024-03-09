5005ebc464fa1fb8980076d74c4021b8460c31c5
r-ca <nem@nem.x0.to>
2024-03-09 18:34:06 +0900

    WIP

--- common/nvim/lua/plugins/toggleterm.lua
+++ common/nvim/lua/plugins/toggleterm.lua
@@ -4,8 +4,15 @@ return {
         config = function()
             require('toggleterm').setup({
                 size = 20,
+				direction = 'float',
+				start_in_insert = true,
+				float_opts = {
+					border = 'curved',
+				}
             })
-			vim.keymap.set('n', 'tr', ':ToggleTerm<CR>')
+			vim.keymap.set('n', '<A-t>', ':ToggleTerm<CR>')
+			vim.keymap.set('t', '<A-t>', '<C-\\><C-n>:ToggleTerm<CR>')
+					
         end
     }
 }
