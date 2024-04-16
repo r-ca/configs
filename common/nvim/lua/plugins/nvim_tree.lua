e271860fa6287632243b080750eb542a89230f1b
ろむねこ <nem@nem.x0.to>
2024-04-16 15:13:52 +0900

    nvim-treeキーマップいろいろ変更

--- common/nvim/lua/plugins/nvim_tree.lua
+++ common/nvim/lua/plugins/nvim_tree.lua
@@ -15,12 +15,13 @@ return {
 				on_attach = function(bufnr)
 					local api = require 'nvim-tree.api'
 					api.config.mappings.default_on_attach(bufnr)
-					vim.keymap.del('n', 'f', { buffer = bufnr })
+					-- vim.keymap.del('n', 'f', { buffer = bufnr })
 					vim.keymap.set('n', '<Space>', api.node.open.edit, { buffer = bufnr })
 					vim.keymap.set('n', '<S-Space>', api.node.open.tab, { buffer = bufnr })
+                    -- vim.keymap.set('n', '<S-Enter>', api.node.open.tab, { buffer = bufnr })
 				end
 			})
-			vim.api.nvim_set_keymap('n', 'f', ':NvimTreeToggle<CR>', { noremap = false, silent = true })
+			vim.api.nvim_set_keymap('n', 'tf', ':NvimTreeToggle<CR>', { noremap = false, silent = true })
 		end
 	}
 }
