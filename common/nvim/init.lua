211a04661c74eb103b307fd8bb1db63c450b8ed5
ろむねこ <nem@nem.x0.to>
2024-04-18 16:17:58 +0900

    LazyGitの起動コマンド変更

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -127,7 +127,7 @@ function _lazygit_toggle()
     lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap('n', 'gg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'tg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
 
 local group = vim.api.nvim_create_augroup("jump_last_position", { clear = true })
 vim.api.nvim_create_autocmd(
